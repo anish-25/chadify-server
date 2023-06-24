4c4eb863b6bde8d27f46a625a976a11212dff977
Anish <anish.menon@openturf.in>
2023-06-24 12:57:45 +0530

    origin

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express()
 
 mongoose.connect(process.env.MONGO_URI,() => console.log("Connected") )
 
-app.use(cors({origin:'https://chadify-client.vercel.app/'}))
+app.use(cors({origin:'https://chadify-client.vercel.app'}))
 app.use(express.json())
 app.use(morgan('common'))
 app.use(helmet())
