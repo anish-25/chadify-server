5d41303424fa9fef8b529b8a0e541ace1fca63e6
Anish <anish.menon@openturf.in>
2023-06-24 13:19:18 +0530

    cookies

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,7 +3,13 @@ const createTokens = (user, req, res) => {
     const accessToken = jwt.sign({ "username": user.name, "id": user.id, "isAdmin": user.isAdmin }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
     const refreshToken = jwt.sign({ "username": user.name, "id": user.id, "isAdmin": user.isAdmin }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' })
     res.status(201)
-    res.cookie("refreshToken", refreshToken, {SameSite:'none' })
+    res.cookie("refreshToken", refreshToken, {
+        domain: 'chadify-client.vercel.app',
+        path:'/',
+        httpOnly: true,
+        secure: true,
+        sameSite:'none'
+         })
     return res.json({
         id: user.id,
         name: user.name,
