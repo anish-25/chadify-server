824bdb2014615a8d710e302b25893af15313f2f7
Anish <anish.menon@openturf.in>
2023-06-24 12:25:13 +0530

    cookie-fix

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,7 +3,7 @@ const createTokens = (user, req, res) => {
     const accessToken = jwt.sign({ "username": user.name, "id": user.id, "isAdmin": user.isAdmin }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
     const refreshToken = jwt.sign({ "username": user.name, "id": user.id, "isAdmin": user.isAdmin }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' })
     res.status(201)
-    res.cookie("refreshToken", refreshToken, { httpOnly: true,secure:true })
+    res.cookie("refreshToken", refreshToken, { httpOnly: true })
     return res.json({
         id: user.id,
         name: user.name,
