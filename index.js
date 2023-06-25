2170075c051f0a9d1fc4b5bbb3daad79356059b5
Anish <anish.menon@openturf.in>
2023-06-25 11:10:36 +0530

    file-upload

--- index.js
+++ index.js
@@ -14,44 +14,44 @@ require('dotenv').config()
 const port = process.env.PORT
 const app = express()
 
-mongoose.connect(process.env.MONGO_URI,() => console.log("Connected") )
+mongoose.connect(process.env.MONGO_URI, () => console.log("Connected"))
 
-app.use(cors({origin:'https://chadify-client.vercel.app',credentials:true}))
+app.use(cors({ origin: 'https://chadify-client.vercel.app', credentials: true }))
 app.use(express.json())
 app.use(morgan('common'))
 app.use(helmet())
 app.use(cookieParser())
 
 const storage = multer.diskStorage({
-    destination: (req,res,cb) => {
-        let path = "public/images/"+req.user.id
-        if(req.body.avatar) path =  "public/images/avatars"
-        if(!fs.existsSync(path)){
-            fs.mkdir(path,() => {
-                cb(null, path);  
+    destination: (req, res, cb) => {
+        let dest = path.join(process.cwd(), "public/images/" + req.user.id)
+        if (req.body.avatar) dest = path.join(process.cwd(), "public/images/avatars")
+        if (!fs.existsSync(dest)) {
+            fs.mkdir(dest, () => {
+                cb(null, dest);
             })
         }
-        else{
-            cb(null, path);
+        else {
+            cb(null, dest);
         }
     },
-    filename: (req,file,cb)=> {
-        cb(null,req.body.name)
+    filename: (req, file, cb) => {
+        cb(null, req.body.name)
     }
 })
 
-const upload = multer({storage})
+const upload = multer({ storage })
 
-app.post('/api/upload',[verifyToken,upload.single("file")],(req, res)=> {
-    try{
-        return res.status(200).json({message:"File uploaded successfully"})
-    }catch(err){
+app.post('/api/upload', [verifyToken, upload.single("file")], (req, res) => {
+    try {
+        return res.status(200).json({ message: "File uploaded successfully" })
+    } catch (err) {
         return res.status(500).json(err)
     }
 })
 
-app.use('/images',express.static(path.join(__dirname,'public/images')))
+app.use('/images', express.static(path.join(__dirname, 'public/images')))
 
-app.use('/api',require('./routes/routes'))
+app.use('/api', require('./routes/routes'))
 
-app.listen(port,console.log(`App running on ${port}`))
+app.listen(port, console.log(`App running on ${port}`))
